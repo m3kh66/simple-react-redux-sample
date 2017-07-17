6232edb8d55099e7c749c79b1b387f454fb61892
Mehdi Golchin <mehdi.golchin.kharazi@gmail.com>
2017-07-17 11:15:15 +0430

    createReducer updates state automatically

--- src/reducers/control.js
+++ src/reducers/control.js
@@ -3,13 +3,8 @@ import { createReducer } from 'reducers';
 import { INCREASE, DECREASE } from 'actions';
 
 const control = createReducer({
-
-    [INCREASE]: (state, { value }) =>
-        Object.assign({}, state, { value: state.value + value }),
-
-    [DECREASE]: (state, { value }) =>
-        Object.assign({}, state, { value: state.value - value })
-
+    [INCREASE]: (state, { value }) => ({ value: state.value + value }),
+    [DECREASE]: (state, { value }) => ({ value: state.value - value })
 }, { value: 0 });
 
 export default control;
--- src/reducers/create-reducer.js
+++ src/reducers/create-reducer.js
@@ -3,6 +3,6 @@ export default function createReducer(def, initState = {}) {
     return function (state = initState, action) {
         let fn = def[action.type];
         if (typeof fn === 'undefined') return state;
-        else return fn(state, action);
+        else return Object.assign({}, state, fn(state, action));
     };
 }
